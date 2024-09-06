e298fda8a7c8b30cef38b11cca54d1d374036ad6
leifehetland <lehetland1@gmail.com>
2024-09-06 13:15:42 -0400

    in progress, edits to starter code

--- 09-Servers-and-APIs/02-Challenge/Develop/server/src/service/weatherService.ts
+++ 09-Servers-and-APIs/02-Challenge/Develop/server/src/service/weatherService.ts
@@ -1,13 +1,58 @@
+import dayjs, { type Dayjs } from 'dayjs';
 import dotenv from 'dotenv';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
+interface Coordinates {
+  name: string;
+  lat: number;
+  lon: number;
+  country: string;
+  state: string;
+}
 
 // TODO: Define a class for the Weather object
+class Weather {
+  city: string;
+  date: Dayjs | string;
+  tempF: number;
+  windSpeed: number;
+  humidity: number;
+  icon: string;
+  iconDescription: string;
+  constructor(
+    city: string,
+    date: Dayjs | string,
+    tempF: number,
+    windSpeed: number,
+    humidity: number,
+    icon: string,
+    iconDescription: string
+  ) {
+    this.city = city;
+    this.date = date;
+    this.tempF = tempF;
+    this.windSpeed = windSpeed;
+    this.humidity = humidity;
+    this.icon = icon;
+    this.iconDescription = iconDescription;
+  }
+}
 
 // TODO: Complete the WeatherService class
 class WeatherService {
-  // TODO: Define the baseURL, API key, and city name properties
+  
+  private baseURL?: string;
+
+  private apiKey?: string;
+
+  private city = '';
+
+  constructor() {
+    this.baseURL = process.env.API_BASE_URL || '';
+
+    this.apiKey = process.env.API_KEY || '';
+  }
   // TODO: Create fetchLocationData method
   // private async fetchLocationData(query: string) {}
   // TODO: Create destructureLocationData method
