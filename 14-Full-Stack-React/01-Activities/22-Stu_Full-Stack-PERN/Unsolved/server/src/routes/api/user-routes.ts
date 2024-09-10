5fd7057f1cad0f7f612b1d46169bec752facf3ef
leifehetland <lehetland1@gmail.com>
2024-09-10 15:15:24 -0400

    updated start code for 14.22 and other edits

--- 14-Full-Stack-React/01-Activities/22-Stu_Full-Stack-PERN/Unsolved/server/src/routes/api/user-routes.ts
+++ 14-Full-Stack-React/01-Activities/22-Stu_Full-Stack-PERN/Unsolved/server/src/routes/api/user-routes.ts
@@ -1,11 +1,20 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import { User } from '../../models/index.js';
 
 const router = express.Router();
 
 // GET /users - Get all users
 router.get('/', async (_req: Request, res: Response) => {
-    //TODO: Get all the Users out of the database and send them to the client as a response
+    // TODO: Get all the Users out of the database and send them to the client as a response
+    try {
+      // * Get all the User data out of the database, excluding password
+   
+      // * Send back the User data as a response to the client
+      
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
 });
 
 export { router as userRouter };
