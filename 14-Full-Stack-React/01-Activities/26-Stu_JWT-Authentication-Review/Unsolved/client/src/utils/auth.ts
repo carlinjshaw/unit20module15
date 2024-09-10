09add3aa16f8b066143b393c3248bf8dddf4a3c3
leifehetland <lehetland1@gmail.com>
2024-09-10 17:11:27 -0400

    updated README for 14.26

--- 14-Full-Stack-React/01-Activities/26-Stu_JWT-Authentication-Review/Unsolved/client/src/utils/auth.ts
+++ 14-Full-Stack-React/01-Activities/26-Stu_JWT-Authentication-Review/Unsolved/client/src/utils/auth.ts
@@ -1,17 +1,39 @@
-//TODO: Import the necessary type and method from 'jwt-decode'
+// * Importing specific types and functions from the 'jwt-decode' library.
+// * JwtPayload: A type definition representing the structure of a JSON Web Token payload.
+// * jwtDecode: A function used to decode a JSON Web Token (JWT) and extract its payload.
+import { type JwtPayload, jwtDecode } from 'jwt-decode';
+import type { UserData } from '../interfaces/UserData';
 
 class AuthService {
   getProfile() {
-    //TODO: Complete this function
+     // Decode the JSON Web Token (JWT) using the jwtDecode function, specifying the expected payload type as UserData.
+    // The getToken() method is called to retrieve the JWT, which is then passed to jwtDecode to extract and return its payload.
+    return jwtDecode<UserData>(this.getToken());
   }
 
   loggedIn() {
     const token = this.getToken();
     return !!token && !this.isTokenExpired(token);
   }
-
+  
+  // TODO: What is causing the token to expire immediately vs waiting until the expiration?
   isTokenExpired(token: string) {
-    //TODO: Complete this function
+    try {
+      // Attempt to decode the provided token using jwtDecode, expecting a JwtPayload type.
+      const decoded = jwtDecode<JwtPayload>(token);
+
+      // We can verify the payload on the token here:
+      console.log(decoded);
+      
+
+      if (decoded?.exp && decoded?.exp < Date.now()) {
+        // If the token is expired, return true indicating that it is expired.
+        return true;
+      }
+    } catch (err) {
+      // If decoding fails (e.g., due to an invalid token format), catch the error and return false.
+      return false;
+    }
   }
 
   getToken(): string {
