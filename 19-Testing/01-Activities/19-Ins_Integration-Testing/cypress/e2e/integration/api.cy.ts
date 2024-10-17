dc75fdb3473efcc4c959d095c0cb780b550b0186
leifehetland <lehetland1@gmail.com>
2024-10-17 17:42:02 -0400

    updated comments on 19.19 api.cy.ts test

--- 19-Testing/01-Activities/19-Ins_Integration-Testing/cypress/e2e/integration/api.cy.ts
+++ 19-Testing/01-Activities/19-Ins_Integration-Testing/cypress/e2e/integration/api.cy.ts
@@ -1,21 +1,22 @@
+// The intention of cy.intercept() is to mock out the network requests
+
 describe('API Requests', () => {
 
-  /*
-  ! Uncomment during demo of incorrect intercept() usage to force all API responses to not be cached
-  beforeEach(() => {
-    cy.intercept(
-      '/api/thought',
-      { middleware: true },
-      (req) => {
-        req.on('before:response', (res) => {
-          // force all API responses to not be cached
-          res.headers['cache-control'] = 'no-store'
-        })
-      }
-    )
-  })
+  
+  // ! Uncomment during demo of incorrect intercept() usage to force all API responses to not be cached
+  // beforeEach(() => {
+  //   cy.intercept(
+  //     '/api/thought',
+  //     { middleware: true },
+  //     (req) => {
+  //       req.on('before:response', (res) => {
+  //         // force all API responses to not be cached
+  //         res.headers['cache-control'] = 'no-store'
+  //       })
+  //     }
+  //   )
+  // })
 
-  */
 
   const mockThoughts = [
     {
@@ -34,3 +35,13 @@ describe('API Requests', () => {
       .should('deep.equal', mockThoughts);
   });
 });
+
+
+// The intention of cy.request() is to be used for checking endpoints on an actual, running server without having to start the front-end application.
+// Similar to what we do in Insomnia
+// describe('API Requests', () => {
+//   it('should GET all thoughts on page load', () => {
+//     cy.visit('https://example.cypress.io')
+//     cy.request('GET', '/api/thought').its('body[0]').should('have.property', 'thoughtText');
+//   });
+// })
